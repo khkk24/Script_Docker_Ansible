a28920943107ce455d1343c75feeac7f54afd2c7
System User <khkk2022@dtic2.cmc.pr.gov.br>
2022-03-31 11:23:46 -0300

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#
+#  Deploiement a la volée de container docker
+#
+#
+
+# Functions
+
+help(){
+echo "
+
+options : 
+	- --create : lancer des containers
+
+	- --drop : supprimer les containers creer par le deploy.sh
+
+	- --infos : caracteristiques des containers (ip, nom, user ...)	
